1c9b7ce7a069c7c52210e413bfb6e1c79369f7bf
Vijayapereira <vijayaipereira@gmail.com>
2022-09-09 12:01:47 -0500

    3rd Commit

---
+++ src/main/java/variousConcepts/GitTest2.java
@@ -0,0 +1,25 @@
+package variousConcepts;
+
+public class GitTest2 {
+
+	public static void main(String[] args) {
+		printme();
+		scriptme();
+		
+	}
+	
+	public static void printme() {
+
+		System.out.println("Git Test 1");
+		System.out.println("Git Test 2");
+		System.out.println("Git Test 3");
+	}
+	
+	public static void scriptme() {
+
+		System.out.println("Git Test 4");
+		System.out.println("Git Test 5");
+		System.out.println("Git Test 6");
+		
+	}
+}
